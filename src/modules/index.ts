b44e7324498add7229cbdc3849bd1511484c9de1
Floris Boom <florisboom1@gmail.com>
2022-01-02 21:17:38 +0100

    Temp shut down sync

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -7,19 +7,19 @@ const app = express();
 
 app.listen(3000);
 
-const syncJob = new CronJob({
-  cronTime: "* 0/2 * * *", // Every second hour
-  async onTick() {
-    try {
-      await SyncService.run();
-    } catch (err) {
-      Logger.log({
-        level: "error",
-        message: `${err}, Failure inside sync job.`,
-      });
-    }
-  },
-  timeZone: "Europe/Amsterdam",
-});
+// const syncJob = new CronJob({
+//   cronTime: "* 0/2 * * *", // Every second hour
+//   async onTick() {
+//     try {
+//       await SyncService.run();
+//     } catch (err) {
+//       Logger.log({
+//         level: "error",
+//         message: `${err}, Failure inside sync job.`,
+//       });
+//     }
+//   },
+//   timeZone: "Europe/Amsterdam",
+// });
 
-syncJob.start();
+// syncJob.start();
